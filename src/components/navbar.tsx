0ce7bc08dc6d46b9699d6d7052f37d07c40d589a
Hítallo William <hitallo91@gmail.com>
2024-04-16 18:02:39 -0300

    fix: remover key errors

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -38,18 +38,15 @@ export const Navbar = (): ReactElement => {
           id="navbar-cta"
         >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            {links.map((link) => (
-              <Link to={link.path}>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                    aria-current="page"
-                  >
-                    {link.label}
-                  </a>
-                </li>
-              </Link>
+            {links.map((link, key) => (
+              <li key={key + 1}>
+                <Link
+                  to={link.path}
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  {link.label}
+                </Link>
+              </li>
             ))}
           </ul>
         </div>
