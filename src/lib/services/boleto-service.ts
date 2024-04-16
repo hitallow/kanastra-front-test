5005dc9a20d16cc7044e716ffcac9049bee1a5ae
Hítallo William <hitallo91@gmail.com>
2024-04-16 16:50:33 -0300

    refactor: adjust boleto service to static methods

--- src/lib/services/boleto-service.ts
+++ src/lib/services/boleto-service.ts
@@ -2,18 +2,24 @@ import { Boleto } from "@/models/boleto";
 import { app } from "../axios";
 
 export class BoletoService {
-  public async loadAll(limit?: number, offset?: number): Promise<Boleto[]> {
+  public static async loadAll(
+    limit?: number,
+    offset?: number
+  ): Promise<{ boletos: Boleto[]; totalItems: number }> {
     try {
       const response = await app.get("/boleto", {
         params: { limit, offset },
       });
       return response.data;
     } catch (error) {
-      return [];
+      return {
+        boletos: [],
+        totalItems: 0,
+      };
     }
   }
 
-  public async loadById(boletoId: string): Promise<Boleto> {
+  public static async loadById(boletoId: string): Promise<Boleto> {
     try {
       const response = await app.get(`/boleto/${boletoId}`);
       return response.data as Boleto;
