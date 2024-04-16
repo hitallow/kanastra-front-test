2a3280c17b487eec1fe809d14c1dd191dbc7ed32
Hítallo William <hitallo91@gmail.com>
2024-04-16 18:12:23 -0300

    feat: add loading

--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -1,12 +1,16 @@
 import { ReactElement } from "react";
 import { Outlet } from "react-router-dom";
+import { Loading } from "../loading";
 import { Navbar } from "../navbar";
+import { useFileContext } from "./file";
 
 function Layout(): ReactElement {
+  const { state: {isLoading} } = useFileContext();
   return (
     <>
       <Navbar />
       <main className="p-6 flex flex-col gap-8">
+        {isLoading && <Loading />}
         <Outlet />
       </main>
     </>
